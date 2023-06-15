ae311b821c464689c324d0e0617b95a5c7314d91
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-06-15 19:48:49 +0300

    Added scrollToSection to header

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
-const burger = document.querySelector(".js-burger-btn");
-const header = document.querySelector(".js-header");
+const burger = document.querySelector('.js-burger-btn');
+const header = document.querySelector('.js-header');
 
-burger.addEventListener("click", () => {
-  header.classList.toggle("active");
+burger.addEventListener('click', () => {
+  header.classList.toggle('active');
 });
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+const menuItems  = document.querySelectorAll('a')
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', (event) => {
+        event.preventDefault()
+        const sectionId = menuItem.getAttribute('href').substring(1)
+        scrollToSection(sectionId)
+    })
+})
